5f8eff5d61e22c875b513d845410380293b86b78
Ricardo Pérez <ricardojpf98@gmail.com>
2017-11-03 17:41:03 -0600

    Added the correct ng-click to view.

--- hola_controller/controller.js
+++ hola_controller/controller.js
@@ -1,6 +1,19 @@
-angular.module("MyFirsApp",[])
-.controller("FirstController", function($scope){
-    $scope.nombre = "Ricardo";
-}).controller("SecondController", function($scope){
-
-});
+angular.module("MyFirsApp", [])
+    .controller("FirstController", function ($scope) {
+        $scope.nombre = "Ricardo";
+        $scope.nuevoComentario = {};
+        $scope.comentarios = [
+            {
+                comentario: "Buen tutorial",
+                username: "ricardoperez"
+        },
+            {
+                comentario: "Mal tutorial",
+                username: "javierfreites"
+        }
+        ];
+        $scope.agregarComentario = function(){
+            $scope.comentarios.push($scope.nuevoComentario);
+            $scope.nuevoComentario = {};
+        }
+    });
