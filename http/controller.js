0377c8bede74e16db64fdceb2fd41f545bc0804e
Ricardo Pérez <ricardojpf98@gmail.com>
2017-11-04 13:45:15 -0600

    Added example of HTTP method POST with Angular.

--- http/controller.js
+++ http/controller.js
@@ -1,7 +1,8 @@
 angular.module("MyFirsApp", [])
     .controller("FirstController", function ($scope, $http) {
         $scope.posts = [];
-        $http.get("http://192.168.0.15:5000/api/v1")
+        $scope.newPost = {};
+        $http.get("https://jsonplaceholder.typicode.com/posts")
             .then(function (datos) {
                     console.log(datos);
                     $scope.posts = datos.data;
@@ -9,4 +10,19 @@ angular.module("MyFirsApp", [])
                 function (error) {
                     console.log(error);
                 });
+        $scope.addPost = function () {
+            $http.post("https://jsonplaceholder.typicode.com/posts", {
+                    title: $scope.newPost.title,
+                    body: $scope.newPost.body,
+                    userId: 1
+                })
+                .then(function (data, status, headers, config) {
+                    console.log(data);
+                    $scope.posts.push($scope.newPost);
+                    $scope.newPost = {};
+
+                }, function (error, status, headers, config) {
+                    console.log(error);
+                });
+        }
     });
