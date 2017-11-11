9eac973b3754ccbd37c011d34d21ab7455a9c8b3
Ricardo Pérez <ricardojpf98@gmail.com>
2017-11-10 22:56:56 -0600

    Added example to my API battle tank

--- http ejemplo con API propia/controller.js
+++ http ejemplo con API propia/controller.js
@@ -1,18 +1,26 @@
 angular.module("MyFirsApp", [])
     .controller("FirstController", function ($scope, $http) {
         $scope.posts = [];
-        var vector = [];
+        $scope.newPost = {};
         $http.get("http://192.168.0.15:5000/api/v1")
             .then(function (datos) {
                     console.log(datos);
                     $scope.posts = datos.data;
-                    $scope.numero = [];
-                    angular.forEach($scope.posts, function (value) {
-                        $scope.numero.push(value.numero);
-                    });
-                    console.log($scope.numero);
                 },
                 function (error) {
                     console.log(error);
                 });
+        $scope.addPost = function () {
+            $http.post("http://192.168.0.15:5000/api/v1", {
+                    usuario: $scope.newPost.usuario
+                })
+                .then(function (data, status, headers, config) {
+                    console.log(data);
+                    $scope.posts.push($scope.newPost);
+                    $scope.newPost = {};
+
+                }, function (error, status, headers, config) {
+                    console.log(error);
+                });
+        };
     });
